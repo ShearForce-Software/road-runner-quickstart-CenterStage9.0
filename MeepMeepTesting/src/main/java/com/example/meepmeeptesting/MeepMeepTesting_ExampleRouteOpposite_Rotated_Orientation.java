eaf068779bdacd9014548b1c13b11df48640b259
Claire-25 <136668541+Claire-25@users.noreply.github.com>
2023-10-10 22:33:50 -0500

    Opposite of MeepMeep Example Route 1 (blue side correct orientation auto route)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting_ExampleRouteOpposite_Rotated_Orientation.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting_ExampleRouteOpposite_Rotated_Orientation.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class MeepMeepTesting_ExampleRouteOpposite { public static void main(String[] args) {
+public class MeepMeepTesting_ExampleRouteOpposite_Rotated_Orientation { public static void main(String[] args) {
     MeepMeep meepMeep = new MeepMeep(600);
 
     RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
@@ -21,12 +21,12 @@ public class MeepMeepTesting_ExampleRouteOpposite { public static void main(Stri
             .setDimensions(17, 15)
             .build();
 
-    myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-60, -36, Math.toRadians(0)))
+    myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-36, 60, Math.toRadians(270)))
 
             // https://rr.brott.dev/docs/v1-0/builder-ref/  Reference for trajectory segments
 
             // move to spike box
-            .splineTo(new Vector2d(-36, -36), Math.toRadians(0))
+            .splineTo(new Vector2d(-36, 36), Math.toRadians(270))
 
 
 
@@ -35,7 +35,7 @@ public class MeepMeepTesting_ExampleRouteOpposite { public static void main(Stri
 //                .splineToLinearHeading(new Pose2d(55, -48, Math.toRadians(270)), Math.toRadians(270))
 
             .setTangent(Math.toRadians(180))
-            .splineToConstantHeading(new Vector2d(-48, -55), Math.toRadians(0))
+            .splineToConstantHeading(new Vector2d(-55, 48), Math.toRadians(270))
 
             // drive forward toward the pixel stacks
             //.splineTo(new Vector2d(15, -55), Math.toRadians(180))
@@ -44,15 +44,15 @@ public class MeepMeepTesting_ExampleRouteOpposite { public static void main(Stri
 //                .setTangent(0)
 //                .splineToConstantHeading(new Vector2d(12, -60), Math.toRadians(270))
 
-            .setTangent(Math.toRadians(0))
-            .splineToLinearHeading(new Pose2d(-12, -60, Math.toRadians(270)), Math.toRadians(270))
+            .setTangent(Math.toRadians(270))
+            .splineToLinearHeading(new Pose2d(-60, 12, Math.toRadians(180)), Math.toRadians(180))
 
 
             //.splineToSplineHeading(new Pose2d(12, -60, Math.toRadians(270)), Math.toRadians(270))
 
             // drive across field toward backdrop
             .setReversed(true)
-            .splineTo(new Vector2d(-12, 24), Math.toRadians(90))
+            .splineTo(new Vector2d(24, 12), Math.toRadians(0))
             .setReversed(false)
 
 //                //constant heading turn 1 to backdrop
@@ -64,52 +64,52 @@ public class MeepMeepTesting_ExampleRouteOpposite { public static void main(Stri
 //                .setTangent(0)
 //                .splineToConstantHeading(new Vector2d(36, 48), Math.toRadians(90))
 
-            .setTangent(Math.toRadians(90))
-            .splineToConstantHeading(new Vector2d(-36, 48), Math.toRadians(90))
+            .setTangent(Math.toRadians(0))
+            .splineToConstantHeading(new Vector2d(48, 36), Math.toRadians(0))
             // manuever to drive under rigging back to pixel stack
-            .setTangent(0)
-            .splineToConstantHeading(new Vector2d(-12,24), Math.toRadians(270))
+            .setTangent(180)
+            .splineToConstantHeading(new Vector2d(24,12), Math.toRadians(180))
 
             // drive to pixel stack for pickup #2
-            .splineTo(new Vector2d(-12, -60), Math.toRadians(270))
+            .splineTo(new Vector2d(-60, 12), Math.toRadians(180))
 
             // drive across field toward backdrop
             .setReversed(true)
-            .splineTo(new Vector2d(-12, 24), Math.toRadians(90))
+            .splineTo(new Vector2d(24, 12), Math.toRadians(0))
 
             //constant heading turn 1 to backdrop
-           // .setTangent(180)
-           // .setReversed(true)
-           // .splineToConstantHeading(new Vector2d(-24, 36), Math.toRadians(90))
+            // .setTangent(180)
+            // .setReversed(true)
+            // .splineToConstantHeading(new Vector2d(-24, 36), Math.toRadians(90))
 
             //constant heading turn 2 to back drop from drop off
-            .setTangent(90)
-            .splineToConstantHeading(new Vector2d(-36, 48), Math.toRadians(90))
+            .setTangent(0)
+            .splineToConstantHeading(new Vector2d(48, 36), Math.toRadians(0))
 
             // manuever to drive under rigging back to pixel stack
-            .setTangent(0)
-            .splineToConstantHeading(new Vector2d(-12, 24), Math.toRadians(270))
+            .setTangent(180)
+            .splineToConstantHeading(new Vector2d(24, 12), Math.toRadians(180))
 
             // drive to pixel stack for pickup #3
-            .splineTo(new Vector2d(-12, -60), Math.toRadians(270))
+            .splineTo(new Vector2d(-60, 12), Math.toRadians(180))
 
             // drive across field toward backdrop
             .setReversed(true)
-            .splineTo(new Vector2d(-12, 24), Math.toRadians(90))
+            .splineTo(new Vector2d(24, 12), Math.toRadians(0))
 
             //constant heading turn 1 to backdrop
-           // .setTangent(90)
-           // .setReversed(true)
-           // .splineToConstantHeading(new Vector2d(-24, 36), Math.toRadians(180))
+            // .setTangent(90)
+            // .setReversed(true)
+            // .splineToConstantHeading(new Vector2d(-24, 36), Math.toRadians(180))
 
             //constant heading turn 2 to back drop from drop off
-            .setTangent(90)
-            .splineToConstantHeading(new Vector2d(-36, 48), Math.toRadians(90))
+            .setTangent(0)
+            .splineToConstantHeading(new Vector2d(48, 36), Math.toRadians(0))
 
             // turn to park in backstage
-            .setTangent(270)
+            .setTangent(180)
             .setReversed(false)
-            .splineToConstantHeading(new Vector2d(-12, 55), Math.toRadians(90))
+            .splineToConstantHeading(new Vector2d(55, 12), Math.toRadians(0))
 
             .build());
 
@@ -124,11 +124,11 @@ public class MeepMeepTesting_ExampleRouteOpposite { public static void main(Stri
             .addEntity(myBot)
             .start();
 */
-       meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
-                .setDarkMode(true)
-                .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
-               .start();
+    meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+            .setDarkMode(true)
+            .setBackgroundAlpha(0.95f)
+            .addEntity(myBot)
+            .start();
 }
 
 }
