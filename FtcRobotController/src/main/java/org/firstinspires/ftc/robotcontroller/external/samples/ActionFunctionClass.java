d6ebf3ed48b6a1d4496bf753b760ccf7240ba880
xavier <xavierzimmerman22@gmail.com>
2023-10-19 19:02:31 -0500

    Tested Spline Test with MeepMeep code.

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ActionFunctionClass.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+//import com.acmerobotics.roadrunner.Action;
+
+
+public class ActionFunctionClass {
+    public Servo left_intake_servo;
+    public Servo right_intake_servo;
+
+  //  left_intake_servo = HardwareMap.get(Servo)
+
+}
--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Aidan_MeepMeepTesting_ExampleRoute3.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Aidan_MeepMeepTesting_ExampleRoute3.java
@@ -6,6 +6,8 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.acmerobotics.roadrunner.Action;
+
 import java.util.concurrent.TimeUnit;
 
 import java.awt.Image;
