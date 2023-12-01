f58d15e4a3f44b2cad51dcf2e3652e9eb8943fe0
jack-jones23 <jacksonjonese57@gmail.com>
2023-11-30 18:34:11 -0600

    trying to fix auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -21,17 +21,15 @@ public class MeepMeepTesting {
                 .setDimensions(17, 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-38.5, -62.5, Math.toRadians(90)))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-38.5,-62.5,Math.toRadians(90)))
 
                 // https://rr.brott.dev/docs/v1-0/builder-ref/  Reference for trajectory segments
                 .splineToLinearHeading(new Pose2d(-38.5, -14.5, Math.toRadians(90)), Math.toRadians(90))
-                        .waitSeconds(1)
-                        .splineToLinearHeading(new Pose2d(-38.5,-10, Math.toRadians(90)), Math.toRadians(90))
-                        .setTangent(0)
-                        .splineToLinearHeading(new Pose2d(-30,-10, Math.toRadians(180)), Math.toRadians(0))
-                        //.setTangent(0)
-                        .splineToLinearHeading(new Pose2d(30,-10, Math.toRadians(180)), Math.toRadians(0))
-                        .splineToLinearHeading(new Pose2d(48,-36, Math.toRadians(180)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(-38,-10, Math.toRadians(90)), Math.toRadians(90))
+                .setTangent(0)
+                .splineToLinearHeading(new Pose2d(-30,-10, Math.toRadians(180)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(30,-10, Math.toRadians(180)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(48,-36, Math.toRadians(180)), Math.toRadians(0))
                 .build());
 
        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
